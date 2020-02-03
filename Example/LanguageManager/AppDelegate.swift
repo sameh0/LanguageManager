c90d030f878672c91402b0fa634070e9344201d4
sameh <samehsayed0@hotmail.com>
2020-02-03 18:23:05 +0200

    update Example

--- Example/LanguageManager/AppDelegate.swift
+++ Example/LanguageManager/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         LanguageManager.defaultLanguage = .en
-        UIButton().supportLanguageDirection
+//        UIButton().supportLanguageDirection
         LanguageManager.firstTimeLanguageSetting()
 
         return true
